759f57b84db203ca81e04561e4f76f74d348f98e
张益诚 <zhangyicheng@jinlianchu.com>
2017-05-19 14:49:58 +0800

    爬取汽车之家

--- src/test/java/com/zyc/AutoHome.java
+++ src/test/java/com/zyc/AutoHome.java
@@ -6,8 +6,16 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+/**
+ * http://www.autohome.com.cn/2488/#levelsource=000000000_0&pvareaid=101594
+ * http://www.autohome.com.cn/3480/#levelsource=000000000_0&pvareaid=101594
+ http://www.autohome.com.cn/2885/#levelsource=000000000_0&pvareaid=101594
+ http://www.autohome.com.cn/2599/#levelsource=000000000_0&pvareaid=101594
+ http://www.autohome.com.cn/2571/#levelsource=000000000_0&pvareaid=101594
+ */
 
 /**
  * Created by zyc on 17/5/19.
@@ -17,42 +25,46 @@ public class AutoHome {
     @Test
     public void test() throws Exception{
         //http://www.autohome.com.cn/car/
-        Document document = Jsoup.connect("http://www.autohome.com.cn/car/").get();
-        Elements select = document.select("a[href]");
+        Document document = Jsoup.connect("http://www.autohome.com.cn/car/").maxBodySize(0).get();
+        Elements select = document.body().select("h4").select("a");
         List<String> carUrlList = new ArrayList();
         for(Element element:select){
             String url = element.attr("href");
+            //http://www.autohome.com.cn/2568/#levelsource=000000000_0&pvareaid=101594
             if(url.contains("www.autohome.com.cn") && url.contains("levelsource=")){
-                carUrlList.add(element.attr("href"));
-                System.out.println(element.attr("href"));
+                carUrlList.add(url);
             }
         }
-        List<String> nameAndPriceList = new ArrayList();
+        FileOutputStream os = new FileOutputStream(new File("/myapp/txt/output.txt"));
+        PrintStream p = new PrintStream(os);
+        p.println("共找到在售车型："+carUrlList.size());
+        int count = 0;
         for(String url :carUrlList){
             Document carDocument = Jsoup.connect(url).timeout(15000).get();
             Elements red = carDocument.select("a[href].red");
-
             for (Element element:red){
                 if(element.attr("href").contains("price.htm")){
                     Elements carName = carDocument.select("h1");
-                    System.out.println("车型："+carName.first().text()+"。报价："+element.text());
-                    nameAndPriceList.add("车型："+carName.first().text()+"。报价："+element.text());
+                    p.println(++count +":车型："+carName.first().text()+"。报价："+element.text());
                 }
             }
         }
+        p.println("实际找到车型个数："+count);
+
+        p.close();
+
 
     }
 
     @Test
     public void test2() throws Exception{
-        Document carDocument = Jsoup.connect("http://www.autohome.com.cn/4208/#levelsource=000000000_0&pvareaid=101594").timeout(10000).get();
+        Document carDocument = Jsoup.connect("http://www.autohome.com.cn/2568/#levelsource=000000000_0&pvareaid=101594").timeout(15000).get();
         Elements red = carDocument.select("a[href].red");
 
         for (Element element:red){
             if(element.attr("href").contains("price.htm")){
-                Elements h1 = carDocument.select("h1");
-                System.out.println(h1.first().text());
-                System.out.println(element.text());
+                Elements carName = carDocument.select("h1");
+                System.out.println(":车型："+carName.first().text()+"。报价："+element.text());
             }
         }
     }
